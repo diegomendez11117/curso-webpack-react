13237242057720e5c4e485f39ecae3bd156d154e
Enrique <diegomendez11117@gmail.com>
2021-03-10 14:28:23 -0800

    finished to upload 5

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/public/index.html',
+            template: './public/index.html',
             filename: 'index.html'
         }),
         new MiniCssExtractPlugin({
